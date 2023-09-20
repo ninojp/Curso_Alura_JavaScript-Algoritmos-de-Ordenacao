bd61b7044c90b5d2a96b5c4833bf63f21b873ae6
Edenilson João Pereira <52428137+ninojp@users.noreply.github.com>
2023-09-20 05:55:21 -0300

    Exibindo os niveis de animhamento do função mergeSort()

--- JavaScript Algoritmos de Ordenação II e Busca/aula-2/index.js
+++ JavaScript Algoritmos de Ordenação II e Busca/aula-2/index.js
@@ -29,3 +29,37 @@ function ordena(parte1, parte2){
     return arrayResultado.concat(posicaoAtualParte1 < parte1.length ? parte1.slice(posicaoAtualParte1) : parte2.slice(posicaoAtualParte2));
 }
 console.log(mergeSort(listaLivros));
+//=====================================================================================================
+
+// EXIBINDO OS NIVEIS DE ANINHAMENTO
+function mergeSort2(array, nivelAninhamento = 0){
+    console.log(`Nivel de Animhamento: ${nivelAninhamento}`);
+    console.log(array);
+    if(array.length > 1){
+        const meio = Math.floor(array.length / 2);
+        const parte1 = mergeSort2(array.slice(0, meio), nivelAninhamento++);
+        const parte2 = mergeSort2(array.slice(meio, array.length), nivelAninhamento++);
+        array = ordena2(parte1, parte2);
+    }
+    return array;
+}
+function ordena2(parte1, parte2){
+    let posicaoAtualParte1 = 0
+    let posicaoAtualParte2 = 0
+    const arrayResultado = []
+
+    while (posicaoAtualParte1 < parte1.length && posicaoAtualParte2 < parte2.length){
+        let produtoAtualParte1 = parte1[posicaoAtualParte1]
+        let produtoAtualParte2 = parte2[posicaoAtualParte2]
+
+        if (produtoAtualParte1.preco < produtoAtualParte2.preco){
+            arrayResultado.push(produtoAtualParte1)
+            posicaoAtualParte1++
+        }else{
+            arrayResultado.push(produtoAtualParte2)
+            posicaoAtualParte2++
+        }
+    }
+    return arrayResultado.concat(posicaoAtualParte1 < parte1.length ? parte1.slice(posicaoAtualParte1) : parte2.slice(posicaoAtualParte2));
+}
+console.log(mergeSort2(listaLivros));
