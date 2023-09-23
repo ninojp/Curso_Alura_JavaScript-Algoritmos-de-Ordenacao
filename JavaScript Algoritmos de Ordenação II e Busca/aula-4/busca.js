b91d84d97344fb65f59c192dfdf665e8fb358dfd
Edenilson João Pereira <52428137+ninojp@users.noreply.github.com>
2023-09-23 15:14:59 -0300

    Update busca.js

--- JavaScript Algoritmos de Ordenação II e Busca/aula-4/busca.js
+++ JavaScript Algoritmos de Ordenação II e Busca/aula-4/busca.js
@@ -21,3 +21,10 @@ console.log(busca(listaLivros, 0, listaLivros.length -1, 40));
 // console.log(busca(listaLivros, 0, listaLivros.length -1, 60)); //RangeError: Maximum call stack size exceeded.
 // console.log(busca(listaLivros, 0, listaLivros.length - 1, 1)); //TypeError: Cannot read property 'preco' of undefined
 // console.log(busca(listaLivros, 0, listaLivros.length - 1, 36)); //RangeError: Maximum call stack size exceeded
+
+// Nesta aula, aprendemos:
+
+// Aplicamos novamente o paradigma “dividir para conquistar” para desenvolver o algoritmo de busca chamado busca binária;
+// Utilizamos recursão para manipular um array ordenado e buscar um elemento, seguindo o fluxo da busca binária que foi visto nas simulações e testes;
+// Desenvolvemos código em JavaScript para implementar o algoritmo de busca binária;
+// Continuamos a prática de funções recursivas, vendo o funcionamento do “caso base”, que pode ser considerada como a condição de parada de uma função recursiva.
